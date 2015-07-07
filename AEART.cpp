a1edd2571abbc7a2fa954a71cd7083f9f6363a13
Sean <lordseancap@gmail.com>
2015-07-07 01:48:18 -0700

    Create AEART.cpp

---
+++ AEART.cpp
@@ -0,0 +1,13 @@
+#ifndef GRAPHICS_INCLUDED
+#define GRAPHICS_INCLUDED
+#define _WIN32_WINNT 0x0500
+#include <windows.h>
+#include "palette.c"
+#include "console_funcs.cpp"
+#include "pixel.h"
+#include "image.h"
+#include "buffer.h"
+#include "gui.cpp"
+#include "input.h"
+
+#endif // GRAPHICS_INCLUDED
